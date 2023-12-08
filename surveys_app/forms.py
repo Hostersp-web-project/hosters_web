c70254f5a6a73492b398a523237656fed37b7713
qkrtmdtj04 <qkrtmdtj04@gmail.com>
2023-12-09 01:44:42 +0900

    radio

--- surveys_app/forms.py
+++ surveys_app/forms.py
@@ -8,4 +8,35 @@ class RoommatePreferencesForm(forms.ModelForm):
     class Meta:
         exclude = ['user']
         model = RoommatePreferences
+        widgets = {
+            'clean_bathroom_periodically': forms.RadioSelect(attrs={'class': 'clean-bathroom'}),
+            'clean_dust_immediately': forms.RadioSelect(attrs={'class': 'clean-dust'}),
+            'vacuum_once_a_day': forms.RadioSelect(attrs={'class': 'vacuum'}),
+            'clean_hair_dust_immediately': forms.RadioSelect(attrs={'class': 'clean-hair-dust'}),
+            'tidiness_waste_energy': forms.RadioSelect(attrs={'class': 'tidiness-waste'}),
+            'tidiness_not_needed': forms.RadioSelect(attrs={'class': 'tidiness-not-needed'}),
+            'Cleaning_Non_Priority_Daily_Life': forms.RadioSelect(attrs={'class': 'cleaning-non-priority'}),
+            'roommate_pet_ok': forms.RadioSelect(attrs={'class': 'roommate-pet'}),
+            'no_friends_without_permission': forms.RadioSelect(attrs={'class': 'no-friends'}),
+            'roommate_drink_less': forms.RadioSelect(attrs={'class': 'roommate-drink-less'}),
+            'Daily_Life_Stress_Susceptibility': forms.RadioSelect(attrs={'class': 'daily-life-stress'}),
+            'late_night_calls_ok': forms.RadioSelect(attrs={'class': 'late-night-calls'}),
+            'long_bathroom_use_ok': forms.RadioSelect(attrs={'class': 'long-bathroom-use'}),
+            'clothes_borrowing_ok': forms.RadioSelect(attrs={'class': 'clothes-borrowing'}),
+            'enter_without_knocking_ok': forms.RadioSelect(attrs={'class': 'enter-without-knocking'}),
+            'meals_with_roommate_important': forms.RadioSelect(attrs={'class': 'meals-with-roommate'}),
+            'roommate_drink_alcohol_well': forms.RadioSelect(attrs={'class': 'roommate-drink-well'}),
+            'emotional_closeness_good': forms.RadioSelect(attrs={'class': 'emotional-closeness'}),
+            'movie_watching_times': forms.RadioSelect(attrs={'class': 'movie-watching'}),
+            'age_difference_ok': forms.RadioSelect(attrs={'class': 'age-difference'}),
+            'same_hobbies_not_needed': forms.RadioSelect(attrs={'class': 'same-hobbies-not-needed'}),
+            'maintain_some_distance': forms.RadioSelect(attrs={'class': 'maintain-distance'}),
+            'Different_Hobbies_Relationship_Impact': forms.RadioSelect(attrs={'class': 'different-hobbies-impact'}),
+            'not_clean_others_mess': forms.RadioSelect(attrs={'class': 'not-clean-others-mess'}),
+            'Self_Responsibility_in_Household_Chores_Effectiveness': forms.RadioSelect(attrs={'class': 'self-responsibility'}),
+            'Individual_Family_Member_Household_Chores_Responsibility': forms.RadioSelect(attrs={'class': 'family-member-responsibility'}),
+            'share_household_chores': forms.RadioSelect(attrs={'class': 'share-chores'}),
+            'Housework_Family_Roles_Over_Individual_Tasks': forms.RadioSelect(attrs={'class': 'family-roles'}),
+        }
+
         # 다른 필드들을 포함하거나 원하는 필드를 지정합니다.
--- surveys_app/models.py
+++ surveys_app/models.py
@@ -3,37 +3,46 @@ from django.conf import settings
 
 class RoommatePreferences(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
-    clean_bathroom_periodically = models.IntegerField()
-    clean_dust_immediately = models.IntegerField()
-    vacuum_once_a_day = models.IntegerField()
-    clean_hair_dust_immediately = models.IntegerField()
-    tidiness_waste_energy = models.IntegerField()
-    tidiness_not_needed = models.IntegerField()
-    Cleaning_Non_Priority_Daily_Life = models.IntegerField()
-    roommate_pet_ok = models.IntegerField()
-    no_friends_without_permission = models.IntegerField()
-    roommate_drink_less = models.IntegerField()
-    Daily_Life_Stress_Susceptibility = models.IntegerField()
-    late_night_calls_ok = models.IntegerField()
-    long_bathroom_use_ok = models.IntegerField()
-    clothes_borrowing_ok = models.IntegerField()
-    enter_without_knocking_ok = models.IntegerField()
-    meals_with_roommate_important = models.IntegerField()
-    roommate_drink_alcohol_well = models.IntegerField()
-    emotional_closeness_good = models.IntegerField()
-    movie_watching_times = models.IntegerField()
-    age_difference_ok = models.IntegerField()
-    same_hobbies_not_needed = models.IntegerField()
-    maintain_some_distance = models.IntegerField()
-    Different_Hobbies_Relationship_Impact = models.IntegerField()
-    not_clean_others_mess = models.IntegerField()
-    Self_Responsibility_in_Household_Chores_Effectiveness = models.IntegerField()
-    Individual_Family_Member_Household_Chores_Responsibility = models.IntegerField()
-    share_household_chores = models.IntegerField()
-    Housework_Family_Roles_Over_Individual_Tasks = models.IntegerField()
+    CLEANING_CHOICES = [
+        (2, 'Strongly Agree'),
+        (1, 'Agree'),
+        (0, 'Neutral'),
+        (-1, 'Disagree'),
+        (-2, 'Strongly Disagree'),
+    ]
+
+    clean_bathroom_periodically = models.IntegerField(choices=CLEANING_CHOICES,default=0)
+    clean_dust_immediately = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    vacuum_once_a_day = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    clean_hair_dust_immediately = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    tidiness_waste_energy = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    tidiness_not_needed = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Cleaning_Non_Priority_Daily_Life = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    roommate_pet_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    no_friends_without_permission = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    roommate_drink_less = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Daily_Life_Stress_Susceptibility = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    late_night_calls_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    long_bathroom_use_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    clothes_borrowing_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    enter_without_knocking_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    meals_with_roommate_important = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    roommate_drink_alcohol_well = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    emotional_closeness_good = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    movie_watching_times = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    age_difference_ok = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    same_hobbies_not_needed = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    maintain_some_distance = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Different_Hobbies_Relationship_Impact = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    not_clean_others_mess = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Self_Responsibility_in_Household_Chores_Effectiveness = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Individual_Family_Member_Household_Chores_Responsibility = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    share_household_chores = models.IntegerField(choices=CLEANING_CHOICES, default=0)
+    Housework_Family_Roles_Over_Individual_Tasks = models.IntegerField(choices=CLEANING_CHOICES, default=0)
 
     class Meta:
         db_table = 'roommate_preferences'
+
 
 
 
