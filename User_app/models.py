2e1cfdc2f3aa2860d8edc3559412fd9dc43838aa
qkrtmdtj04 <qkrtmdtj04@gmail.com>
2023-12-10 21:03:50 +0900

    user add

---
+++ User_app/models.py
@@ -0,0 +1,24 @@
+
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    university = models.CharField(max_length=255)
+    major = models.CharField(max_length=255)
+    bedtime = models.IntegerField()
+    wake_up_time = models.IntegerField()
+    time_of_move_in = models.IntegerField()
+    phone_number_1 = models.CharField(max_length=255)
+    phone_number_2 = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'User'
